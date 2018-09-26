2f0c2f2f7249fbde2d64e57a857d8d25ba8b58ab
Peter Wurzinger <wurzingerp@gmail.com>
2018-09-26 19:36:14 +0200

    Tests for CommandQueue

--- test/CL.Core.Tests.Unit/Model/CommandQueueTests.cs
+++ test/CL.Core.Tests.Unit/Model/CommandQueueTests.cs
@@ -38,7 +38,7 @@ namespace CL.Core.Tests.Unit.Model
         }
 
         [Fact]
-        public void CtorShouldThrowExceptionIfInteropCallUnsuccessfull()
+        public void CtorShouldThrowExceptionIfInteropCallUnsuccessful()
         {
             var platform = new Platform(IntPtr.Zero, FakeOpenClApi);
             var device = new Device(platform, IntPtr.Zero, FakeOpenClApi.DeviceApi);
@@ -60,7 +60,153 @@ namespace CL.Core.Tests.Unit.Model
             Assert.Equal(new IntPtr(2), cq.Id);
         }
 
-        //TODO: Tests for Command Queue properties
+        [Fact]
+        public void CtorShouldSetDevice()
+        {
+            var platform = new Platform(new IntPtr(1), FakeOpenClApi);
+            var device = new Device(platform, new IntPtr(1), FakeOpenClApi.DeviceApi);
+            var context = new Context(FakeOpenClApi, new[] { device });
 
+            var cq = new CommandQueue(context, device, false, false, FakeOpenClApi.CommandQueueApi);
+
+            Assert.NotNull(cq.Device);
+            Assert.Equal(device, cq.Device);
+        }
+
+        [Fact]
+        public void CtorShouldSetContext()
+        {
+            var platform = new Platform(new IntPtr(1), FakeOpenClApi);
+            var device = new Device(platform, new IntPtr(1), FakeOpenClApi.DeviceApi);
+            var context = new Context(FakeOpenClApi, new[] { device });
+
+            var cq = new CommandQueue(context, device, false, false, FakeOpenClApi.CommandQueueApi);
+
+            Assert.NotNull(cq.Context);
+            Assert.Equal(device, cq.Device);
+        }
+
+        [Fact]
+        public void DisposeShouldNotThrowExceptionIfDisposedMultipleTimes()
+        {
+            var platform = new Platform(new IntPtr(1), FakeOpenClApi);
+            var device = new Device(platform, new IntPtr(1), FakeOpenClApi.DeviceApi);
+            var context = new Context(FakeOpenClApi, new[] { device });
+            var cq = new CommandQueue(context, device, false, false, FakeOpenClApi.CommandQueueApi);
+
+            cq.Dispose();
+            cq.Dispose();
+        }
+
+        [Fact]
+        public void GetHashCodeShouldReturnHashCodeOfId()
+        {
+            var platform = new Platform(new IntPtr(1), FakeOpenClApi);
+            var device = new Device(platform, new IntPtr(1), FakeOpenClApi.DeviceApi);
+            var context = new Context(FakeOpenClApi, new[] { device });
+
+            var cq = new CommandQueue(context, device, false, false, FakeOpenClApi.CommandQueueApi);
+
+            Assert.Equal(cq.Id.GetHashCode(), cq.GetHashCode());
+        }
+
+        [Fact]
+        public void EqualsShouldReturnFalseIfOtherIsNull()
+        {
+            var platform = new Platform(new IntPtr(1), FakeOpenClApi);
+            var device = new Device(platform, new IntPtr(1), FakeOpenClApi.DeviceApi);
+            var context = new Context(FakeOpenClApi, new[] { device });
+
+            var cq = new CommandQueue(context, device, false, false, FakeOpenClApi.CommandQueueApi);
+
+            Assert.False(cq.Equals(null));
+        }
+
+        [Fact]
+        public void EqualsShouldReturnTrueIfOtherIsSameReference()
+        {
+            var platform = new Platform(new IntPtr(1), FakeOpenClApi);
+            var device = new Device(platform, new IntPtr(1), FakeOpenClApi.DeviceApi);
+            var context = new Context(FakeOpenClApi, new[] { device });
+
+            var cq = new CommandQueue(context, device, false, false, FakeOpenClApi.CommandQueueApi);
+            var other = cq;
+
+            Assert.True(cq.Equals(other));
+        }
+
+        [Fact]
+        public void EqualsShouldReturnTrueIfCommandQueuesHaveSameIds()
+        {
+            var platform = new Platform(new IntPtr(1), FakeOpenClApi);
+            var device = new Device(platform, new IntPtr(1), FakeOpenClApi.DeviceApi);
+            var context = new Context(FakeOpenClApi, new[] { device });
+
+            FakeOpenClApi.FakeCommandQueueApi.clCreateCommandQueueResult = new IntPtr(2);
+            var cq = new CommandQueue(context, device, false, false, FakeOpenClApi.CommandQueueApi);
+            var other = new CommandQueue(context, device, false, false, FakeOpenClApi.CommandQueueApi);
+
+            Assert.True(cq.Equals(other));
+        }
+
+        [Fact]
+        public void EqualsShouldReturnFalseIfObjIsNull()
+        {
+            var platform = new Platform(new IntPtr(1), FakeOpenClApi);
+            var device = new Device(platform, new IntPtr(1), FakeOpenClApi.DeviceApi);
+            var context = new Context(FakeOpenClApi, new[] { device });
+
+            var cq = new CommandQueue(context, device, false, false, FakeOpenClApi.CommandQueueApi);
+
+            Assert.False(cq.Equals((object)null));
+        }
+
+        [Fact]
+        public void EqualsShouldReturnTrueIfObjIsSameReference()
+        {
+            var platform = new Platform(new IntPtr(1), FakeOpenClApi);
+            var device = new Device(platform, new IntPtr(1), FakeOpenClApi.DeviceApi);
+            var context = new Context(FakeOpenClApi, new[] { device });
+
+            var cq = new CommandQueue(context, device, false, false, FakeOpenClApi.CommandQueueApi);
+            object other = cq;
+
+            Assert.True(cq.Equals(other));
+        }
+
+        [Fact]
+        public void EqualsShouldReturnTrueIfObjIsOfSameTypeAndHasSameId()
+        {
+            var platform = new Platform(new IntPtr(1), FakeOpenClApi);
+            var device = new Device(platform, new IntPtr(1), FakeOpenClApi.DeviceApi);
+            var context = new Context(FakeOpenClApi, new[] { device });
+
+            FakeOpenClApi.FakeCommandQueueApi.clCreateCommandQueueResult = new IntPtr(1);
+            var cq = new CommandQueue(context, device, false, false, FakeOpenClApi.CommandQueueApi);
+            object other = new CommandQueue(context, device, false, false, FakeOpenClApi.CommandQueueApi);
+
+            Assert.True(cq.Equals(other));
+        }
+
+        [Fact]
+        public void EqualsShouldReturnFalseIfObjIsOfDifferentType()
+        {
+            var platform = new Platform(new IntPtr(1), FakeOpenClApi);
+            var device = new Device(platform, new IntPtr(1), FakeOpenClApi.DeviceApi);
+            var context = new Context(FakeOpenClApi, new[] { device });
+
+            var cq = new CommandQueue(context, device, false, false, FakeOpenClApi.CommandQueueApi);
+            object other = new SuspiciousCommandQueue(context, device, false, false, FakeOpenClApi.FakeCommandQueueApi);
+
+            Assert.False(cq.Equals(other));
+        }
+
+        //TODO: Tests for Command Queue properties
+        private class SuspiciousCommandQueue : CommandQueue
+        {
+            internal SuspiciousCommandQueue(Context context, Device device, bool enableProfiling, bool enableOutOfOrderExecutionMode, ICommandQueueApi commandQueueApi) : base(context, device, enableProfiling, enableOutOfOrderExecutionMode, commandQueueApi)
+            {
+            }
+        }
     }
 }
