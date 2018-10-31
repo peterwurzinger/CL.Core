6da010853da245b0f36b397947a8b130bc463bca
Wurzinger Peter <Peter.Wurzinger@noel.gv.at>
2018-10-31 14:05:31 +0100

    Optimized InfoHelper

--- src/CL.Core/API/InfoHelper.cs
+++ src/CL.Core/API/InfoHelper.cs
@@ -1,6 +1,5 @@
 ﻿using CL.Core.Model;
 using System;
-using System.Runtime.InteropServices;
 using System.Text;
 
 namespace CL.Core.API
@@ -21,46 +20,47 @@ namespace CL.Core.API
             _infoFunc = infoFunc ?? throw new ArgumentNullException(nameof(infoFunc));
         }
 
-        public TValue GetValue<TValue>(TParameter parameterName)
-            where TValue : struct
-        {
-            var bytes = GetBytes(parameterName);
-            return MemoryMarshal.Read<TValue>(bytes);
-        }
-
-        public ReadOnlySpan<TValue> GetValues<TValue>(TParameter parameterName)
-            where TValue : struct
-        {
-            var bytes = GetBytes(parameterName);
-            return MemoryMarshal.Cast<byte, TValue>(bytes);
-        }
-
-        public string GetStringValue(TParameter parameterName, Encoding encoding)
-        {
-            var bytes = GetBytes(parameterName);
-            return encoding.GetString(bytes.ToArray());
-        }
-
-        private unsafe ReadOnlySpan<byte> GetBytes(TParameter parameterName)
+        public unsafe TValue GetValue<TValue>(TParameter parameterName)
+            where TValue : unmanaged
         {
             var error = _infoFunc(_entity.Id, parameterName, 0, IntPtr.Zero, out var paramSize);
             error.ThrowOnError();
 
+            var stackMemory = stackalloc TValue[1];
+            error = _infoFunc(_entity.Id, parameterName, paramSize, (IntPtr)stackMemory, out _);
+            error.ThrowOnError();
 
-            var memory = new ReadOnlyMemory<byte>(new byte[paramSize]);
-            var memoryHandle = memory.Pin();
+            return stackMemory[0];
+        }
 
-            try
+        public unsafe ReadOnlySpan<TValue> GetValues<TValue>(TParameter parameterName)
+            where TValue : unmanaged
+        {
+            var error = _infoFunc(_entity.Id, parameterName, 0, IntPtr.Zero, out var paramSize);
+            error.ThrowOnError();
+
+            //Explicitly allocating an array. Stackalloc doesn't make sense, since it would get allocated on heap eventually on return
+            var memory = new TValue[(int)paramSize / sizeof(TValue)];
+
+            fixed (void* ptr = memory)
             {
-                error = _infoFunc(_entity.Id, parameterName, paramSize, new IntPtr(memoryHandle.Pointer), out _);
+                error = _infoFunc(_entity.Id, parameterName, paramSize, (IntPtr)ptr, out _);
                 error.ThrowOnError();
+            }
 
-                return memory.Span;
-            }
-            finally
-            {
-                memoryHandle.Dispose();
-            }
+            return memory;
+        }
+
+        public unsafe string GetStringValue(TParameter parameterName, Encoding encoding)
+        {
+            var error = _infoFunc(_entity.Id, parameterName, 0, IntPtr.Zero, out var paramSize);
+            error.ThrowOnError();
+
+            var stackMemory = stackalloc byte[(int)paramSize];
+            error = _infoFunc(_entity.Id, parameterName, paramSize, (IntPtr)stackMemory, out _);
+            error.ThrowOnError();
+
+            return encoding.GetString(stackMemory, (int)paramSize);
         }
     }
 }
